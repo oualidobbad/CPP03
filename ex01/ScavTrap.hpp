2e41c57b4fb41377e3b7f2f345a6fd613fcda234
User <user@192.168.2.199>
2025-10-31 20:23:50 +0100

    done

---
+++ ex01/ScavTrap.hpp
@@ -0,0 +1,13 @@
+#include "ClapTrap.hpp"
+
+class ScavTrap: public ClapTrap
+{
+	public:
+		ScavTrap();
+		ScavTrap(std::string name);
+		ScavTrap(const ScavTrap& scavtrap);
+		ScavTrap& operator=(const ScavTrap& scavTrap);
+		void attack(const std::string& target);
+		void guardGate();
+		~ScavTrap();
+};
---
+++ ex01/main.cpp
@@ -0,0 +1,9 @@
+#include "ScavTrap.hpp"
+
+int main ()
+{
+	ScavTrap child("Alice");
+	child.attack("Rice");
+	child.beRepaired(3);
+	return 0;
+}
